a0eea23781ea4408b7266791b8bcb924e5367952
ramon sepulveda <monche_9@hotmail.com>
2018-08-23 12:48:10 -0500

    compatibility only with mongo 3.4 and add initial user

--- ui/server/config/accounts.js
+++ ui/server/config/accounts.js
@@ -3,3 +3,11 @@ Accounts.emailTemplates.from = "Kadira <no-reply@kadira.io>";
 Accounts.emailTemplates.resetPassword.subject = function () {
   return i18n("emails.reset_password_subject");
 };
+if (Meteor.users.find({roles:["admin"]}).fetch().length === 0) {
+  var userObj = {
+    email:"admin@admin.com",
+    password:"admin",
+    profile:{name:"admin"}
+  };
+  Accounts.createUser(userObj);
+}
